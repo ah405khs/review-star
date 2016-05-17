d63ace97f33928d434048466beeab5ce46e3dc9c
missahng <awr0607@gmail.com>
2016-05-17 05:06:44 +0000

    that snack show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,10 +6,14 @@ class HomeController < ApplicationController
     end
     
     def show
+        @snack = Snack.find(params[:id]) #r과자 하나 찾아서 던져주기#여기서 id는 마법의 routes에서 설정해준거
         
     end
     
     def new
     
+    end
+
+    def update
     end
 end
